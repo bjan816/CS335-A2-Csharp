04dd52edcb82a9bb4edfe3c0c9dfd8285281773e
Kellie-J <bjan816@aucklanduni.ac.nz>
2023-08-25 23:22:02 +1200

    Move ConvertEventToICalendar into CalendarOutputFormatter

--- Controllers/A2Controller.cs
+++ Controllers/A2Controller.cs
@@ -3,6 +3,7 @@ using System.Text;
 using A2.Data;
 using A2.Dtos;
 using A2.Handler;
+using A2.Helper;
 using A2.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -98,33 +99,6 @@ namespace A2.Controllers
             return Ok(count);
         }
 
-        public static string ConvertEventToICalendar(Event e)
-        {
-            var dtStamp = DateTime.UtcNow;
-
-            var builder = new StringBuilder();
-
-            const string uid = "bjan816";
-
-            builder.AppendLine("BEGIN:VCALENDAR");
-            builder.AppendLine($"PRODID:-//{uid}");
-            builder.AppendLine("VERSION:2.0");
-            builder.AppendLine("BEGIN:VEVENT");
-
-            builder.AppendLine($"UID:{e.Id}");
-            builder.AppendLine($"DTSTAMP:{dtStamp:yyyyMMddTHHmmssZ}");
-            builder.AppendLine($"DTSTART:{e.Start:yyyyMMddTHHmmssZ}");
-            builder.AppendLine($"DTEND:{e.End:yyyyMMddTHHmmssZ}");
-            builder.AppendLine($"SUMMARY:{e.Summary}");
-            builder.AppendLine($"DESCRIPTION:{e.Description}");
-            builder.AppendLine($"LOCATION:{e.Location}");
-
-            builder.AppendLine("END:VEVENT");
-            builder.AppendLine("END:VCALENDAR");
-
-            return builder.ToString();
-        }
-
         [Authorize(Policy = "HasAuthority")]
         [HttpGet("Event/{id}")]
         public async Task<IActionResult> Event(int id)
@@ -135,7 +109,7 @@ namespace A2.Controllers
                 return BadRequest($"Event {id} does not exist.");
             }
 
-            var iCalendarContent = ConvertEventToICalendar(e);
+            var iCalendarContent = CalendarOutputFormatter.ConvertEventToICalendar(e);
             return Ok(iCalendarContent);
         }
     }
--- Helper/CalendarOutputFormatter.cs
+++ Helper/CalendarOutputFormatter.cs
@@ -1,1 +1,35 @@
-﻿
+﻿using System.Text;
+using A2.Models;
+
+namespace A2.Helper
+{
+    class CalendarOutputFormatter
+    {
+        public static string ConvertEventToICalendar(Event e)
+        {
+            var dtStamp = DateTime.UtcNow;
+
+            var builder = new StringBuilder();
+
+            const string uid = "bjan816";
+
+            builder.AppendLine("BEGIN:VCALENDAR");
+            builder.AppendLine($"PRODID:-//{uid}");
+            builder.AppendLine("VERSION:2.0");
+            builder.AppendLine("BEGIN:VEVENT");
+
+            builder.AppendLine($"UID:{e.Id}");
+            builder.AppendLine($"DTSTAMP:{dtStamp:yyyyMMddTHHmmssZ}");
+            builder.AppendLine($"DTSTART:{e.Start:yyyyMMddTHHmmssZ}");
+            builder.AppendLine($"DTEND:{e.End:yyyyMMddTHHmmssZ}");
+            builder.AppendLine($"SUMMARY:{e.Summary}");
+            builder.AppendLine($"DESCRIPTION:{e.Description}");
+            builder.AppendLine($"LOCATION:{e.Location}");
+
+            builder.AppendLine("END:VEVENT");
+            builder.AppendLine("END:VCALENDAR");
+
+            return builder.ToString();
+        }
+    }
+}
