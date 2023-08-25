fc3f2de3de9cb84107edb92ded1bd955c80081eb
Name <name@email.com>
2023-08-25 22:57:51 +1200

    Remove required from name and price

--- Models/Product.cs
+++ Models/Product.cs
@@ -13,9 +13,9 @@ namespace A2.Models
         }
 
         [Key] public int Id { get; set; }
-        [Required] public string Name { get; set; }
-        [Required] public float Price { get; set; }
-        
+        public string Name { get; set; }
+        public float Price { get; set; }
+
         [Required] public string Description { get; set; }
     }
 }
